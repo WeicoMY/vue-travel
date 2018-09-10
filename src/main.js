3ab9fd88b07c9349c5c31e5a8ae21ccd1fc59647
myweico <470398956@qq.com>
2018-09-10 15:19:43 +0800

    add home-swiper

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import router from './router'
+import fastClick from 'fastclick'
 
 import 'styles/main.styl'
+// vue-awesome-swiper styles
+import 'swiper/dist/css/swiper.css'
+
+// use global swiper
+Vue.use(VueAwesomeSwiper)
+
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="swiper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="url of urls" :key="url">
+      <img :src="url" alt="swiper-images" class="swiper-image">
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 3000
+      },
+
+      urls: [
+        'http://dimg04.c-ctrip.com/images/700a0v000000jy2fyA1FD_1080_216_25.jpg',
+        'http://dimg04.c-ctrip.com/images/70050w000000ka9ib1476_1080_216_25.jpg'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.swiper >>> .swiper-pagination-bullet-active
+  background #fff
+.swiper
+    width 100%
+    overflow hidden
+    height 0
+    padding-bottom 20%
+    .swiper-image
+      width 100%
+</style>
